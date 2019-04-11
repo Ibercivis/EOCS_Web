3251242a394ddf8f2f3d27cd59a5c070b625f532
Victor <vval@bifi.es>
2019-04-11 10:45:01 +0000

    Upload file component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadFileComponent } from './upload-file/upload-file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFileComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/upload-file/upload-file.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.css']
+})
+export class UploadFileComponent implements OnInit {
+
+  uploadFileForm;
+  requirements;
+  private contentFile;
+
+  constructor() {
+    this.uploadFileForm= new FormGroup({
+      file: new FormControl('')
+    });
+   }
+
+  ngOnInit() {
+
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    this.requirements = this.contentFile;
+    console.log(this.requirements);
+  }
+
+  onFileChange(event) {
+    let reader = new FileReader();
+    if(event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsText(file);
+      reader.onloadend = (e) => {
+        //console.log(reader.contentFile);
+        this.contentFile = reader.result;
+        this.contentFile = this.contentFile.split(/\n/);
+     };
+    }
+  }
+
+}
