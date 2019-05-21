391cd04e2d0e5465cc573c3a623e3dd1fc6fef5c
Victor <vval@bifi.es>
2019-05-21 12:24:52 +0000

    Box filter for text requirement

--- src/app/requirements/requirements.component.ts
+++ src/app/requirements/requirements.component.ts
@@ -11,6 +11,7 @@ import { RequirementsService } from '../requirements.service';
 export class RequirementsComponent implements OnInit {
 
   requirements:Requirement[] = [];
+  filteredOptions: Requirement[] = [];
   searchRequirementForm;
   selectProjectForm;
   sortType = 'text';
@@ -18,38 +19,50 @@ export class RequirementsComponent implements OnInit {
   accounts;
   selectedAccount;
   defaultAccount = 'ibercivis';
+  
 
   constructor(private requirementService : RequirementsService) {
-    this.searchRequirementForm= new FormGroup({
-      searchRequirement: new FormControl('')
-    });
     this.selectProjectForm= new FormGroup({
       selectedAccount: new FormControl('')
     });
+    this.searchRequirementForm= new FormGroup({
+      searchRequirement: new FormControl('')
+    });
     this.selectProjectForm.controls['selectedAccount'].setValue(this.defaultAccount, {onlySelf: true});
-
   }
 
   ngOnInit() {
-    this.getRequirementsByProject(this.defaultAccount);
     this.getProjects();
+    this.getRequirementsByProject(this.defaultAccount);
+    this.searchRequirementForm.controls['searchRequirement'].valueChanges
+    .subscribe(
+      value => {
+      console.log('Filter value changed:' + value);
+      this.filterRequirements();
+      }
+    );    
   }
 
   getProjects(){
     this.requirementService.getProjects().subscribe(apiData => (this.accounts = apiData));
   }
 
-  getRequirements() {
-    this.requirementService.getRequirements().subscribe(apiData => (this.requirements = apiData));
-  }
-  getRequirementsByProject(project) {
-    this.requirementService.getRequirementsByProject(project).subscribe(apiData => (this.requirements = apiData));
-  }
-
   accountChange(value){
     var account = value.substring(value.indexOf(' ')+ 1,value.length);
     console.log(account);
     this.getRequirementsByProject(account);
+  }
+
+  getRequirements() {
+    this.requirementService.getRequirements().subscribe(apiData => (this.requirements = apiData));
+  }
+  getRequirementsByProject(project) {
+    this.requirementService.getRequirementsByProject(project).subscribe(apiData => (this.filteredOptions = this.requirements = apiData));
+  }
+
+  filterRequirements(){
+    this.filteredOptions = this.requirements.filter(requirement =>
+      requirement.text.toLowerCase().indexOf(this.searchRequirementForm.controls['searchRequirement'].value.toLowerCase()) !== -1);
   }
   
 }
